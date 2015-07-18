d970194664d063387d3ac0e4cb717b11c5920036
Utku Yücel <utku.yucel@t2.com.tr>
2015-07-18 21:32:03 +0300

    initial commit

---
+++ app/src/main/java/com/wakeup/lms/letmesleep/AlarmActivity.java
@@ -0,0 +1,92 @@
+package com.wakeup.lms.letmesleep;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+
+public class AlarmActivity extends Activity {
+
+    AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
+    private TimePicker alarmTimePicker;
+    private static AlarmActivity inst;
+    private TextView alarmTextView;
+
+    public static AlarmActivity instance() {
+        return inst;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // Enable Local Datastore.
+//        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "fmm5KHgyhaVzy5TniU90vtwkNJuXEs1A4nJL7NdZ", "ZL8qaqYczQjYFbwSYcrWBzGLKomPeInKbqBYYIjz");
+
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+
+        alarmTimePicker = (TimePicker) findViewById(R.id.alarmTimePicker);
+        alarmTextView = (TextView) findViewById(R.id.alarmText);
+        ToggleButton alarmToggle = (ToggleButton) findViewById(R.id.alarmToggle);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+    }
+
+    public void onToggleClicked(View view) {
+        if (((ToggleButton) view).isChecked()) {
+            Log.d("MyActivity", "Alarm On");
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+            calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+            Intent myIntent = new Intent(AlarmActivity.this, AlarmReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this, 0, myIntent, 0);
+            alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
+        } else {
+            alarmManager.cancel(pendingIntent);
+            setAlarmText("");
+            Log.d("MyActivity", "Alarm Off");
+        }
+    }
+
+    public void setAlarmText(String alarmText) {
+        alarmTextView.setText(alarmText);
+    }
+}
+
+
+
